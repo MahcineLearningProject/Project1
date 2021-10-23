a0d94e93cb865618255e1649cdb0bc141ed12489
jmontavon <92521026+jmontavon@users.noreply.github.com>
2021-10-23 13:58:11 +0200

    Update implementations.py

--- implementations.py
+++ implementations.py
@@ -2,6 +2,7 @@ import numpy as np
 from proj1_helpers import *
 
 def build_poly_plus_missing(tX, degree, missing_vectors):
+    N = tX.shape[0]
     polx = build_poly(tX, degree)
     polx = polx.reshape(N, -1)
     polx = np.append(polx, missing_vectors, axis = 1)
