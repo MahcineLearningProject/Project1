834a14489add712201f36bde88fd2370885722d4
kbr2020 <kbr2020@hotmail.com>
2021-10-31 22:10:06 +0100

    big change in run
    patched a conceptual error

--- Rendu/run.py
+++ Rendu/run.py
@@ -11,9 +11,10 @@ def main():
     DATA_TRAIN_PATH = './data/train.csv' # TODO: download train data and supply path here 
     y, tX, ids = load_csv_data(DATA_TRAIN_PATH)
 
+    tx_tr,tx_te,y_tr,y_te = split_data(tX, y, 0.9,4)
 
-    X, Y, missing_vectors = divide_set(tX,y)
-
+    X_tr, Y_tr, missing_vectors = divide_set(tx_tr,y_tr)
+    X_te, Y_te, missing_vectors = divide_set(tx_te,y_te)
 
     weightss = []
     degrees = []
@@ -27,12 +28,20 @@ def main():
 
     for i in range(4):
         print("index :" + str(i))
-        x_n = X[i]
-        y_n = Y[i]
-        x,col = cleanse_data(x_n)
-        x,mean,std = standardize(x)
 
-        min_degree, min_weight,min_error = find_best(x,y_n,degress,lambdas,missing_vectors[i])
+        x_tr_new = X_tr[i]
+        y_tr = Y_tr[i]
+
+        x_te_new = X_te[i]
+        y_te = Y_te[i]
+
+        x_tr,col = cleanse_data(x_tr_new)
+        x_tr ,mean,std = standardize(x_tr)
+
+        x_te = cleanse_data_col(x_te_new,col)
+        x_te = standardize_with_info(x_te,mean,std)
+
+        min_degree, min_weight,min_error = find_best(x_tr,y_tr, x_te, y_te,degress,lambdas,missing_vectors[i])
         print("found the min error" + str(min_error) + "  for this index")
         weightss.append(min_weight)
         degrees.append(min_degree)
@@ -52,9 +61,7 @@ def main():
         X_test = cleanse_data_col(X_t[i],cols[i])
         X_test = standardize_with_info(X_test,Means[i],STDS[i])
         X_test_poly = build_poly(X_test,degrees[i])
-        print(X_test_poly.shape)
         y_a = predict_labels(weightss[i], X_test_poly)
-        print(y_a.shape)
         Y_pred[ tX_test[:,22] == i] = y_a
 
 
@@ -63,16 +70,18 @@ def main():
     print('FINISHED')
 
 
-def find_best(tx,y,degrees, lambdas,missing_vector):
+def find_best(x_tr,y_tr,x_te,y_te,degrees, lambdas,missing_vector):
 
     min_degree = degrees[0]
     min_weight = np.array({})
     min_error = 1
 
     for deg in degrees:
-        tx_poly = build_poly(tx,deg)
+        tx_tr = build_poly(x_tr,deg)
+
+        tx_te = build_poly(x_te,deg)
         #tx_poly = np.concatenate((missing_vector, tx_poly),1)
-        tx_tr,tx_te,y_tr,y_te = split_data(tx_poly, y, 0.9,4)
+            
         index = 0
         for lambda_ in lambdas:
             weights_r, error_r =  ridge_regression(y_tr,tx_tr,lambda_)
